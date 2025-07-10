2cd5a56ac86e79fd29085eee5c14596ecf3de6eb
m m <kmkl>
2025-07-10 18:57:01 -0400

    First commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// const express = require("express")
+import express from "express"
+
+const app = express()
+
+app.get("/", (req,res)=> {
+    res.send("It's working");
+});
+
+app.listen(5001, () => {
+    console.log("Server is up and running on PORT:5001");
+});
+
